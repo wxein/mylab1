28aa3e748b1e84e30c51a8e16474d2df1e2f496f
wxein <54906066+wxein@users.noreply.github.com>
2021-01-19 17:04:33 +0800

    Update FileQueryController.java

--- FileQueryController.java
+++ FileQueryController.java
@@ -1,52 +1,123 @@
-package com.example.demo.pojo;
+package com.dbs.celerity.queryrunner.api.filequery;
 
-//java bean
-public class Tree {
-    private int id;
-    private int pId;
-    private String name;
-    private String url;
+import com.dbs.celerity.queryrunner.pojo.Tree;
+import com.netflix.config.DynamicStringProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.*;
 
-    public int getId() {
-        return id;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+
+public class FileQueryController {
+    List <Tree>node=new LinkedList();
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileQueryController.class);
+    private static final DynamicStringProperty DIRECTORY_PATH = new DynamicStringProperty("filequery.directorypath.url", null);
+
+    @RequestMapping("file")
+    public List<Tree> file() throws UnsupportedEncodingException {
+        LOGGER.info("Started File Query Process");
+        node.removeAll(node);
+        //String path=DIRECTORY_PATH.get();
+        String path="./";
+        int level=0;
+        List<Tree>file=getFile(path,1,level);
+        LOGGER.info("Contents for file query" + file);
+        return file;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    private  List<Tree> getFile(String path,int id,int pid) throws UnsupportedEncodingException {
+        File file = new File(path);
+        if(file.exists()) {
+            File[] array = file.listFiles();
+
+            List fileList = Arrays.asList(array);
+
+            Collections.sort(fileList, new Comparator<File>() {
+                @Override
+                public int compare(File o1, File o2) {
+                    if (o1.isDirectory() && o2.isFile())
+                        return -1;
+                    if (o1.isFile() && o2.isDirectory())
+                        return 1;
+                    return o1.getName().compareTo(o2.getName());
+                }
+            });
+
+            for (int i = 0; i < array.length; i++) {
+                Tree tree = new Tree();
+                tree.setpId(pid);
+                tree.setId(id);
+                tree.setName(array[i].getName());
+
+                //check whether it is directory
+                if (array[i].isDirectory()) {
+                    node.add(tree);
+                    getFile(array[i].getPath(), id * 10 + 1 + i, id);
+                    id++;
+                } else {
+                    tree.setUrl("/file/download?filePath="+java.net.URLEncoder.encode(array[i].getAbsolutePath(),"utf-8").replace("\\","/"));
+                    node.add(tree);
+                    id++;
+                }
+            }
+        } else {
+            System.out.println("File not exist");
+        }
+        return node;
     }
 
-    public int getpId() {
-        return pId;
-    }
+    @RequestMapping("file/download")
+    public String download(HttpServletRequest request, HttpServletResponse response, String filePath){
+        File fileurl = new File(filePath);
 
-    public void setpId(int pId) {
-        this.pId = pId;
-    }
+        String showValue =filePath.substring(filePath.lastIndexOf("/")+1);;
+        System.out.println(showValue);
+        try{
+            InputStream inStream = new FileInputStream(fileurl);
 
-    public String getName() {
-        return name;
-    }
+            final String userAgent = request.getHeader("USER-AGENT");
 
-    public void setName(String name) {
-        this.name = name;
-    }
+            String finalFileName = null;
+            if(StringUtils.contains(userAgent, "MSIE")||StringUtils.contains(userAgent,"Trident")){
+                finalFileName = URLEncoder.encode(showValue,"UTF8");
 
-    public String getUrl() {
-        return url;
-    }
+            }else if(StringUtils.contains(userAgent, "Mozilla")){
+                finalFileName = new String(showValue.getBytes(), "ISO8859-1");
+            }else{
+                finalFileName = URLEncoder.encode(showValue,"UTF8");
+            }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+            response.reset();
+            response.setContentType("application/x-download");
+            response.addHeader("Content-Disposition" ,"attachment;filename=\"" +finalFileName+ "\"");
 
-    @Override
-    public String toString() {
-        return "Tree{" +
-                "id=" + id +
-                ", pid=" + pId +
-                ", name='" + name + '\'' +
-                ", url='" + url + '\'' +
-                '}';
+
+            byte[] b = new byte[1024];
+            int len;
+            while ((len = inStream.read(b)) > 0){
+                response.getOutputStream().write(b, 0, len);
+            }
+            inStream.close();
+            response.getOutputStream().close();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
-
